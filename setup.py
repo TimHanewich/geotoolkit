457302eeba71bff653c1b1ef2ec04b78abfe8d47
Tim Hanewich <tahanewich@live.com>
2023-04-30 13:48:17 -0400

    Incremented to version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_desc:str = f.read()
 
 setup(name='geotoolkit',
-    version='0.2.0',
+    version='0.2.1',
     description='Peforming calculations with geographic points',
     long_description=long_desc,
     long_description_content_type='text/markdown',
