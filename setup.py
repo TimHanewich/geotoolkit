27e9b52b88689256207b44b4e359ca639ed697f1
Tim Hanewich <tahanewich@live.com>
2023-04-30 13:41:04 -0400

    Added readme to package

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 import setuptools
 
+with open("readme.md", "r") as f:
+    long_desc:str = f.read()
+
 setup(name='geotoolkit',
     version='0.1.0',
     description='Peforming calculations with geographic points',
+    long_description=long_desc,
     author='Tim Hanewich',
     url='https://github.com/TimHanewich/geotoolkit',
     packages=setuptools.find_packages()
